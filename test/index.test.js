56531e3d5680d8bdc3e9cb42661fedf96fcfd5fb
marc bassil <marc.bassil1@gmail.com>
2019-02-24 10:57:53 -0500

    cleaning up test constants

--- test/index.test.js
+++ test/index.test.js
@@ -8,6 +8,9 @@ nock.disableNetConnect()
 
 describe("argo-cd-bot", () => {
     let probot
+    // constants
+    const argoCDToken = "token"
+    const argoCDServer = "1.2.3.4"
     
     beforeEach(() => {
         probot = new Probot({})
@@ -15,26 +18,30 @@ describe("argo-cd-bot", () => {
         app.app = () => "test"
 
         // node env variables
-        process.env.ARGO_CD_API_TOKEN = "token";
-        process.env.ARGO_CD_SERVER_IP = "1.2.3.4"
+        process.env.ARGO_CD_API_TOKEN = argoCDToken
+        process.env.ARGO_CD_SERVER_IP = argoCDServer
     })
 
-    // I"m not sure how to properly fix this
     test("diff comment posted on PR", async() => {
         nock("https://api.github.com")
             .post("/app/installations/2/access_tokens")
             .reply(200, {token: "test"})
 
+        // test constants
         const branch = "newBranch"
+        const appDiff = "===== App Diff ===="
+        const appName = "app1"
+        const appDir = "projects/app1"
+
         nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch}}})
 
         const child_process = require("child_process")
         const execStub = sinon.stub(child_process, "exec")
+        // first exec, will fork script to clone repo
         execStub.onCall(0).yields(false)
 
-        const appDiff = "===== App Diff ===="
-        nock("https://1.2.3.4").get("/api/v1/applications?fields=items.metadata.name,items.spec.source.path,items.spec.source.repoURL")
-            .reply(200, {"items": [{"metadata": { "name": "app1" }, "spec": { "source": { "path": "dir1" } } }] })
+        nock("https://" + argoCDServer).get("/api/v1/applications?fields=items.metadata.name,items.spec.source.path,items.spec.source.repoURL")
+            .reply(200, {"items": [{"metadata": { "name": appName }, "spec": { "source": { "path": appDir } } }] })
         execStub.onCall(1).yields(false, {"stdout": appDiff})
         // regex match post body should match diff produced by API
         nock("https://api.github.com").post("/repos/robotland/test/issues/109/comments", /===== App Diff ====/).reply(200)
