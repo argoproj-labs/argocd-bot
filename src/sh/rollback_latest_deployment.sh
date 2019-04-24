39dcef2a372d037966730664ce674f19f4e11789
marc bassil <marc.bassil1@gmail.com>
2019-04-24 08:36:20 -0400

    minor fixes to rollback

--- src/sh/rollback_latest_deployment.sh
+++ src/sh/rollback_latest_deployment.sh
@@ -4,26 +4,47 @@
 # syncs app with revision=branch
 
 function usage_and_exit() {
-    echo "${0} [action: run, view] [app name]"
+    echo "${0} [optional flag: -d or --dry-run] [app name]"
     exit 1
 }
 
-function view_latest_deployment() {
+function view_deployments() {
     local app_name=${1}
     argocd app history ${app_name} --plaintext
 }
 
-action="${1}"
-app_name="${2}"
+if [[ ${1} == "--dry-run" || ${1} == "-d" ]]; then
+    dry_run=true
+    app_name="${2}"
+else
+    app_name="${1}"
+fi
 
-if [[ -z "${app_name}" || -z "${action}" ]]; then
+if [[ -z "${app_name}" ]]; then
     usage_and_exit
 fi
 
-if [[ ${action} == "run" ]]; then
-    # get the first of the last two deployments, as we need to revert the latest one, by going to the deployment before the latest
-    latest=$(view_latest_deployment ${app_name} | tail -2 | head -1 | awk '{print $1}')
-    argocd app rollback ${app_name} ${latest} --plaintext
-elif [[ ${action} == "view" ]]; then
-    view_latest_deployment ${app_name} | sed -n '1p;$p'
+history_count=$(view_deployments ${app_name} | sed  '1d' | wc -l)
+if [[ ${history_count} -lt 1 ]]; then
+    echo "There is less than one deployment made via argo, cannot rollback; 2 or more deployments must exist:"
+    echo "Deployment history:"
+    view_deployments ${app_name}
+    exit
+fi
+
+# remove the first line (which is just the status line)
+# the latest deployment is the last line in the history output
+latest=$(view_deployments ${app_name} | sed  '1d' | tail -1)
+# the deployment before the last is the one we want to roll back to
+before_latest=$(view_deployments ${app_name} | sed  '1d' | tail -2 | head -1 )
+
+if [[ ${dry_run} = true ]]; then
+    echo "Latest deployment:"
+    echo "${latest}"
+    echo ""
+    echo "Will rollback the above deployment to the following:"
+    echo "${before_latest}"
+else
+    roll=$(echo $before_latest | awk '{print $1}')
+    argocd app rollback ${app_name} ${roll} --plaintext
 fi
