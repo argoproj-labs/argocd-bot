d852301910e24ec6c57ed3c5202cf890e3800619
marc bassil <marc.bassil1@gmail.com>
2019-05-06 11:50:53 -0400

    version 0.2

--- helper_scripts/build_and_push.sh
+++ helper_scripts/build_and_push.sh
@@ -2,7 +2,7 @@
 set -e
 # helper script to push to docker hub: https://hub.docker.com/r/marcb1/argocd-bot
 
-version=0.1
+version=0.2
 docker build -f Dockerfile -t argocd-bot .
 docker tag argocd-bot marcb1/argocd-bot:v${version}
 docker push marcb1/argocd-bot:v${version}
